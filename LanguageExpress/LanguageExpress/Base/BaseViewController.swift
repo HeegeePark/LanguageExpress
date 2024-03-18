167adaa8d2012ecd27c53b3c798f0979a74d9736
kizi <phg980513@naver.com>
2024-03-18 23:35:43 +0900

    [추가] #1: BaseView 추가 및 관련 프로토콜 정의

--- LanguageExpress/LanguageExpress/Base/BaseViewController.swift
+++ LanguageExpress/LanguageExpress/Base/BaseViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class BaseViewController: UIViewController {
+class BaseViewController: UIViewController, ViewDrawable {
     
     override func viewDidLoad() {
         super.viewDidLoad()
